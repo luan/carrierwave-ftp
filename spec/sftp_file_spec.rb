9b69fac96e166d3b1e5fc9485155dfe7113123ab
M. Scott Ford <scott@corgibytes.com>
2018-02-28 16:04:03 -0500

    Fixes undefined method exception when querying content_type
    Fixes:
    
    ```
    NameError:
     undefined local variable or method `file' for #<CarrierWave::Storage::SFTP::File:0x0000000002ddc190>
    ```
    
    and:
    
    ```
    NameError:
     undefined local variable or method `file' for #<CarrierWave::Storage::FTP::File:0x0000000002833978>
    ```
    
    Updates implementation to use the same content type inferrence behavior
    that is built into CarrierWave.

---
+++ spec/sftp_file_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'carrierwave/storage/sftp'
+
+class SftpUploader < CarrierWave::Uploader::Base
+  storage :sftp
+end
+
+describe CarrierWave::Storage::SFTP::File do
+  let(:uploader) do
+    Class.new(CarrierWave::Uploader::Base) do
+      storage :sftp
+    end.tap do |u|
+      allow(u).to receive(:store_path).and_return('uploads/test.jpg')
+    end
+  end
+
+  let(:base) { CarrierWave::Storage::SFTP.new(uploader) }
+
+  let(:file) do
+    CarrierWave::Storage::SFTP::File.new(uploader, base, uploader.store_path)
+  end
+
+  let(:mime_type) { double('mime_type') }
+
+  describe '#content_type' do
+    it 'delegates to base file by default' do
+      sanitized_file = CarrierWave::SanitizedFile.new(file)
+      expect(CarrierWave::SanitizedFile).to receive(:new).with(file.path).
+        and_return(sanitized_file)
+      expect(sanitized_file).to receive(:content_type).and_return(mime_type)
+
+      expect(file.content_type).to eq(mime_type)
+    end
+
+    it 'permits overriding the default value' do
+      file.content_type = mime_type
+
+      expect(file.content_type).to eq(mime_type)
+    end
+  end
+end
